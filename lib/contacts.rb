06dedfc94a3088fd1b6d489a8a5b70453e963995
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 23:55:52 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,17 +1,19 @@
 require 'pry'
 
- require_relative "../lib/contacts.rb"
+  # This is the array we will be passing into the remove_strawberry method
+  # contacts = {
+  #   "Jon Snow" => {
+  #     name: "Jon",
+  #     email: "jon_snow@thewall.we", 
+  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  #   },
+  #   "Freddy Mercury" => {
+  #     name: "Freddy",
+  #     email: "freddy@mercury.com",
+  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #   }
+  # }
+  
+def remove_strawberry(contacts)
 
-puts remove_strawberry(contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we", 
-      favorite_icecream_flavors: ["chocolate", "vanilla"]
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
-  })
 end
